a7b7c6b13cc60811fcb12f7fd88743d3d52cc448
imer alejandro <consymer1@gmail.com>
2024-03-11 18:26:34 -0700

    configuracion de movil instalada

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -7,6 +8,43 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      manifest: {
+        name: 'linkify',
+        short_name: 'linkify',
+        description: 'Una aplicacion para organizar y registrar enlaces',
+        start_url: '/',
+        display: 'fullscreen',
+        background_color: '#164773',
+        theme_color: '#012340',
+        icons: [
+          {
+            src: "/molecular.png",
+            sizes: "250x250",
+            type: "image/png"
+          },
+          {
+            src:"/icono-192-192.png",
+            sizes:"192x192",
+            type:"image/png",
+            purpose: 'maskable',
+          },
+          
+        ]
+      },
+      // share_target: {
+      //   action: "/compartir",
+      //   method: "GET",
+      //   enctype: "application/x-www-form-urlencoded",
+      //   params: {
+      //     url: "url"
+      //   }
+      // },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      }
+    })
   ],
   resolve: {
     alias: {
